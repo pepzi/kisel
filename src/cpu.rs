9d6cc3c8ae694735eed9f4e005d9a282b00c7e8a
Robert <766765+pepzi@users.noreply.github.com>
2026-09-08 01:04:34 +0200

    Add 16-bit register pair helpers (BC, DE, HL, AF)

--- src/cpu.rs
+++ src/cpu.rs
@@ -32,6 +32,46 @@ impl Cpu {
         }
     }
 
+    pub fn get_bc(&self) -> u16 {
+        ((self.b as u16) << 8) | (self.c as u16)
+    }
+
+    pub fn set_bc(&mut self, value: u16) {
+        self.b = (value >> 8) as u8;
+        self.c = (value & 0xFF) as u8;
+    }
+
+    pub fn get_de(&self) -> u16 {
+        ((self.d as u16) << 8) | (self.e as u16)
+    }
+
+    pub fn set_de(&mut self, value: u16) {
+        self.d = (value >> 8) as u8;
+        self.e = (value & 0xFF) as u8;
+    }
+
+    // HL-pair (the Game Boys most important register pair for memory pointers)
+    pub fn get_hl(&self) -> u16 {
+        (self.h as u16) << 8 | (self.l as u16)
+    }
+
+    pub fn set_hl(&mut self, value: u16) {
+        self.h = (value >> 8) as u8;
+        self.l = (value & 0xFF) as u8;
+    }
+
+    // AF-pair (seldome used for direct calculations, but needed for stack
+    // operations)
+    pub fn get_af(&self) -> u16 {
+        (self.a as u16) << 8 | (self.f as u16)
+    }
+
+    pub fn set_af(&mut self, value: u16) {
+        self.a = (value >> 8) as u8;
+        // NOTICE: The lowest four bits of the F register are ALWAYS zero!
+        self.f = (value & 0xF0) as u8;
+    }
+
     // Run one instruction and return how many clock cycles it took
     pub fn step(&mut self, mmu: &mut Mmu) -> u32 {
         let opcode = mmu.read_byte(self.pc);
